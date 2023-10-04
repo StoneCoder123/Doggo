039c16ba4eb2cde46d71f56c8df4b86eecbd8e47
StoneZoo <shikhars2003@gmail.com>
2023-10-05 02:55:32 +0530

    Committed some files

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.getElementById('Doggo-container')
+
+
+
+document.getElementById('btn').addEventListener('click', () => {
+    const promise = fetch('https://dog.ceo/api/breeds/image/random');
+    alert("Fetching...");
+    promise.then(res => {
+        const process = res.json();
+        alert("Processing...");
+        return process;
+    }).then(process => {
+        const img = document.createElement('img')
+        img.src = process.message;
+        img.alt = "Cute Doggo";
+        alert("Happy Dogging!")
+        container.appendChild(img);
+    })
+})
